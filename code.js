a06306c67c39e4344e9456156593d730a1dc6a3e
interestinall <justin.lefurjah@gmail.com>
2016-07-22 09:40:47 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favoriteIceCream(name) {
+  return `I love ${name}`;
+};
+
+function shouting(str) {
+  return str.toUpperCase();
+};
+
+function roundDown(num) {
+  return Math.floor(num);
+};
+
+function theTruth() {
+  return true
+};
