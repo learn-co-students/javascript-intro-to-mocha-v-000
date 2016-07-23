6f6dbb92b87c4dff4faa05dfb71ef0070ddfd36f
Jennifer Pazos <jmp521@gmail.com>
2016-07-23 06:49:02 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favoriteIceCream(name) {
+    return `I love ${name}`
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  var new_number = Math.floor(number);
+  return new_number;
+}
+
+function theTruth() {
+  return true;
+}
