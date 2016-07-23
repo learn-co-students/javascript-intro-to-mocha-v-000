62460bca64eb52610bbac6e535c6544faa67919e
azemoh <joshazemoh@gmail.com>
2016-07-23 01:48:46 +0100

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`;
+}
+
+function shouting(text) {
+  return text.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
