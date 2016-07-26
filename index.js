15116c962c47945c7e15a1f23b2e051fbae2b04e
Jason Casares <jasonc@csdrafting.com>
2016-07-26 20:34:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-test
+function favoriteIceCream(flavor) {
+  return "I love " + flavor + ".";
+}
+
+function shouting(s) {
+  return s.toUpperCase()
+}
+
+function roundDown(n) {
+  return Math.floor(n)
+}
+
+function theTruth() {
+  return true
+}
