f5b88dd3c5510caebdcc9367d37e6911303481d0
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-07-28 20:49:19 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+	return true;
+}
+
+function sayHey(){
+	return "Hey!"
+}
+
+function sayHeyFriend(friend){
+	return "Hey, "+friend+"!";
+}
+
+function favoriteIceCream(icecream){
+	return "I love "+icecream;
+}
+
+function shouting(input){
+	return input.toUpperCase();
+}
+
+function roundDown(x){
+	return Math.floor(x);
+}
