4e0e22fa8ea06b37cd1275c81983122a557820d3
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-08-08 11:15:36 -0700

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!";
+}
+
+function sayHeyFriend(name){
+  return "Hey, " + name +"!";
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(string){
+  // var string = "I love javascript soooooo much";
+  return string.toUpperCase();
+}
+
+function roundDown(numb){
+  return Math.floor(numb);
+}
