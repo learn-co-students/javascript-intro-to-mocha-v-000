83b04185a5df1a67f960d7008ee937e812891fdf
Vannak Chhay <vannak.chhay1@gmail.com>
2016-08-13 11:50:37 -0400

    done

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favoriteIceCream(icecream){
+  return "I love #{icecream}";
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(integer){
+  return Math.floor(integer);
+}
+
+function theTruth(){
+  return true;
+}
