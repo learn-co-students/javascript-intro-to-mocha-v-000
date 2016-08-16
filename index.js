c60332fa6efad4ed5e74d97ddf34a9d98e7385e9
Josh Avina <josh.avina78@gmail.com>
2016-08-16 14:59:56 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth() {
+  return true;
+};
+
+function sayHey() {
+  return "Hey!";
+};
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+};
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+};
+
+function shouting(v) {
+  var caps = v.toUpperCase();
+  return caps;
+};
+
+function roundDown(num) {
+  var number = Math.floor(num);
+  return number;
+}
