c3344c423f243c926a79653367d476ed361c9ac3
tttra <tothutra@gmail.com>
2016-08-30 17:27:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(name) {
+  return `I love ${name}`
+}
+
+function shouting(phase) {
+  var result = phase.toUpperCase()
+  return result
+}
+
+function roundDown(number) {
+  var result = Math.floor(number)
+  return result
+}
