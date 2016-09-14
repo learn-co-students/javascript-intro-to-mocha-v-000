b0943cf985c75d85d061f8fa147bc70e4c3fd90c
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-14 16:28:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return `Hey!`;
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`;
+}
+
+function favoriteIceCream(icecream) {
+  return `I love ${icecream}`;
+}
+
+function shouting(string) {
+  var shout = string.toUpperCase();
+  return shout;
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
