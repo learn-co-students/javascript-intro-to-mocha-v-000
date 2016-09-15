01249094510c77e6cac4dd8d1f5029aaa3e9fdd9
Megan Eloise <mev277@nyu.edu>
2016-09-15 13:42:39 +0200

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function theTruth() {
+  return true;
+}
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name + "!";
+}
+
+function favoriteIceCream(cream) {
+  return "I love " + cream ;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
