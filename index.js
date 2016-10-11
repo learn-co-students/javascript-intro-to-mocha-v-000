c237aeeed15909c6c3025a0fac41ae7775b90a7f
James Novak <jamesnovak90@gmail.com>
2016-10-10 23:12:33 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var theTruth = function() {
+  return true
+}
+
+var sayHey = function() {
+  return 'Hey!'
+}
+
+var sayHeyFriend = function(name) {
+  return 'Hey, ' + name + '!'
+}
+
+var favoriteIceCream = function(icecream) {
+  return "I love " + icecream
+}
+
+var shouting = function(string) {
+  return string.toUpperCase()
+}
+
+var roundDown = function(num) {
+  return Math.floor(num)
+}
