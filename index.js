a32f790e94714231598e624883344c9a008fa2a4
DEREJE GEMECHU <dereje88@gmail.com>
2016-10-13 03:52:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+
+}
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(favorite) {
+  if (favorite == "mint chocolate chip"){
+      return `I love mint chocolate chip`
+  }
+  return 0;
+}
+function shouting(text) {
+  return `${text.toUpperCase()}`;
+}
+
+
+function roundDown(num) {
+  return Math.floor(num);
+}
