e9de6429b2551dfc24e0f950b37dcc706650b35c
Adrian Dave <adrian@paradoxlogic.co>
2016-10-28 17:20:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return 'Hey, ' + name + '!';
+}
+
+function favoriteIceCream(flavor) {
+  return 'I love ' + 'mint chocolate chip';
+}
+
+function shouting(wordsToShout) {
+  return wordsToShout.toUpperCase();
+}
+
+function roundDown(numberToFloor) {
+  return Math.floor(numberToFloor);
+}
