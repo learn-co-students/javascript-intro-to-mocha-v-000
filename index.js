5210624fd8f0b81a241c8782cb4aa523bfc7bc91
cody krauskopf <ckrausko@gmail.com>
2016-10-31 22:24:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth(){
+  return true;
+}
+function sayHey(){
+  return 'Hey!';
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(type){
+  return `I love ${type}`;
+}
+function shouting(words){
+  return words.toUpperCase();
+}
+function roundDown(number){
+  return Math.floor(number);
+}
