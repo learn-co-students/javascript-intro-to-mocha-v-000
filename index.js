3a82f40123c1196f6b192ed344aaf6cfdf9abe18
gormanjp <jpgorman8@gmail.com>
2016-11-03 23:50:50 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var theTruth = function(){
+  return true;
+}
+
+var sayHey = function(){
+  return 'Hey!';
+}
+
+var sayHeyFriend = function(name){
+  return 'Hey, ' + name + '!';
+}
+
+var favoriteIceCream = function(ic){
+  return 'I love ' + ic;
+}
+
+var shouting = function(shout){
+  return shout.toUpperCase();
+}
+
+var roundDown = function(num){
+  return Math.floor(num);
+}
