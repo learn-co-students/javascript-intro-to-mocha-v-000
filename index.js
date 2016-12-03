965e96940c4c227ba4f51fb1a14b533c1ebab6c5
lizdenhup <lizdenhup@gmail.com>
2016-12-02 21:45:16 -0600

    finished intro to testing with mocha

---
+++ index.js
@@ -0,0 +1,25 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function roundDown(float) {
+  var rounded = Math.floor(float);
+  return rounded; 
+}
+
+function shouting(string) {
+  var shout = string.toUpperCase();
+  return shout; 
+}
