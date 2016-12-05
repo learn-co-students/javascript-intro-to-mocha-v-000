4146c72650cc1a8181af62840bc8997007a53c0f
Jennifer Green <greenjenniferb@gmail.com>
2016-12-05 23:35:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+
+function favoriteIceCream(iceCream) {
+  return `I love ${iceCream}`;
+}
+
+function shouting(capsString) {
+  return capsString.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
