9f7cebc2e206e1b2b5b050d66362da11edc9bbab
bshap93 <bshap93@gmail.com>
2016-12-09 13:04:48 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  debugger;
+  return `Hey, ${name}!`;
+}
+
+function shouting(word) {
+  return word.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
