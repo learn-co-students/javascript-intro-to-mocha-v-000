b34235740fb07c350e0a93238952127fe4d82215
lcorr8 <l.corr.v@gmail.com>
2016-12-15 23:06:45 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(icecream) {
+  return `I love ${icecream}`
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function shouting(shout) {
+  return shout.toUpperCase();
+}
