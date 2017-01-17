6923c0d327f073cd2bcafbd8543245e8b0d9145b
Heber Sandoval <he3ers@gmail.com>
2017-01-17 16:01:38 -0500

    Use mocha and debugger to test code.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(sayWhat) {
+  // debugger;
+  return sayWhat.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
