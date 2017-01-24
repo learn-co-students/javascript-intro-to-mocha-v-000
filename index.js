4f4ddd508f0ae8ff81077944176d9a4fe99f7846
Mason Ellwood <mason.ellwood@freestar.io>
2017-01-24 01:53:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name + "!";
+}
+
+function favoriteIceCream(name) {
+  return "I love " + name;
+}
+
+/*function shouting() {
+  return "i love javascript soooooo much".toUpperCase();
+}*/
+
+function shouting(greeting) {
+  return greeting.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
