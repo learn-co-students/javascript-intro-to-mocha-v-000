cac6f5a31b90fc7218ef7c5336877030e7a9d3ca
johnram528 <johnram528@gmail.com>
2017-02-16 10:34:12 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theTruth(){
+  return true;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function shouting(strg){
+  return strg.toUpperCase()
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+
