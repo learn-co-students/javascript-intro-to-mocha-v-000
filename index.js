423bd95d56dac65474ccf1e1fbb9b3e0f64f5c4b
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-03-26 21:33:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(friendName) {
+  return `Hey, ${friendName}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(quote) {
+  return quote.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
