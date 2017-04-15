cb2f4d994959ffef523641579c3d163bd53c490a
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-15 21:07:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function theTruth() {
+  return true 
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(int) {
+  return Math.floor(int)
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`
+}
