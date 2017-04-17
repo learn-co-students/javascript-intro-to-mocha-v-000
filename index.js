e7b2cb60363835ccd46dacd66ad093f3ca3ae61f
Brian Holland <beehollander@gmail.com>
2017-04-17 15:48:04 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name){
+  return "Hey, " + name + "!"
+}
+
+function favoriteIceCream(flava){
+      return `I love ${flava}`
+}
+ 
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(n){
+  return Math.floor(n);
+}
