3d4ad00f53ef18b83076a47d0a5d081b45097355
warrend <warrend555@gmail.com>
2017-05-22 14:53:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+	return true;
+}
+
+function sayHey() {
+	return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+	return `Hey, ${name}!`
+}
+
+function favoriteIceCream() {
+	return `I love ${arguments[0]}`
+}
+
+function shouting() {
+	return arguments[0].toUpperCase()
+}
+
+function roundDown() {
+	return Math.floor(arguments[0])
+}
