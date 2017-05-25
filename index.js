dfe17fbf6e39f754e37c069c2d8a2261076d85ca
James Dolan <jwdolan820@gmail.com>
2017-05-25 03:32:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(s) {
+  return s.toUpperCase()
+}
+
+function roundDown(n) {
+  return Math.floor(n)
+}
+
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+   return 'Hey!';
+ }
+
+ function sayHeyFriend(name) {
+   return `Hey, ${name}!`;
+ }
