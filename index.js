41caf76c4a534d231c0211c0670f0ff282980411
nicholasbair <bairinc@gmail.com>
2017-07-13 19:01:22 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const theTruth = () => {
+  return true;
+};
+
+const sayHey = () => {
+  return 'Hey!';
+};
+
+const sayHeyFriend = (name) => {
+  return `Hey, ${name}!`;
+};
+
+const favoriteIceCream = (name) => {
+  return `I love ${name}`;
+};
+
+const shouting = (str) => {
+  return str.toUpperCase();
+};
+
+const roundDown = (n) => {
+  return Math.floor(n);
+}
