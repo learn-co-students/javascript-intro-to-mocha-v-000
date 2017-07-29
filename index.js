f664ba3d5ab298ff0f1df83831597330681a8fda
lucketta <lucketta@umich.edu>
2017-07-28 21:38:10 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var theTruth = () => {
+  return true;
+}
+
+var sayHey = () => {
+  return "Hey!";
+}
+
+var sayHeyFriend = (name) => {
+  return `Hey, ${name}!`;
+}
+
+var favoriteIceCream = (flavor) => {
+  return `I love ${flavor}`;
+}
+
+var shouting = (str) => {
+  return str.toUpperCase();
+}
+
+var roundDown = (num) => {
+  return Math.floor(num)
+}
