f819e6e9d1d18ca3f885d1ebc810af7e8285ccdf
Howard DeVennish <howarddevennish@gmail.com>
2017-07-30 14:29:38 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function favoriteIceCream(flavor) {
+  debugger
+  return `I love ${flavor}`
+}
+
+function theTruth() {return true}
+
+function sayHey() {return "Hey!"}
+
+function sayHeyFriend(name) {return `Hey, ${name}!`}
+
+function shouting(string) {return string.toUpperCase()}
+
+function roundDown(number) {return Math.floor(number)}
