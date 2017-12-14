75acc1481580d52cdeb705360fa50b393c5b92a3
John Tuttle <thejwt@gmail.com>
2017-12-14 20:00:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(friend) {
+  return `Hey, ${friend}!`;
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function shouting(shout) {
+  return shout.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
