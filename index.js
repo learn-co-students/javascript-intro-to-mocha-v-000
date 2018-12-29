f9a4312f897001c3aa0e0d38daced778b06d0a0b
github username <github email address>
2018-12-28 22:03:35 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+// describe('roundDown()', function () {
+//   it('should correctly floor the number', function () {
+//     expect(roundDown(5.99)).toEqual(5);
+//   });
+// });
+// });
